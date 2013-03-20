f4580f29b25c5922458485629a93158fc7ef3d12
arrogantrobot <arrogantrobot@gmail.com>
2013-03-19 20:02:56 -0500

    Add git pull updating to maintenance class

--- box/maintenance.py
+++ box/maintenance.py
@@ -12,10 +12,10 @@ class Maintenance(FarawayBoxConfig):
         update_cmd = self.update_cmd()
         print "running update command {}".format(" ".join(update_cmd))
         update_process = subprocess.Popen(cmd, cwd=daemonDir)
-        update_process.wait()
+        if update_process.wait() != 0:
+            raise IOError("updating git repo failed")
 
     def update_cmd(self):
         remote = self.attrs["get_update"][remote]
         branch = self.attrs["get_update"][branch]
         return ["git", "pull", remote, branch]
-
